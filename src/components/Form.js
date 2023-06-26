ff54dce0b10dfcdb827ddd8b182d442cf2a471b6
Francella Fiallos <francella.fiallos@gmail.com>
2023-06-26 11:29:42 -0300

    added a reset button, some responsiveness work

--- src/components/Form.js
+++ src/components/Form.js
@@ -132,6 +132,7 @@ const Form = () => {
                     <div className="button-container">
 
                         <button type="submit">tell me!</button>
+                        <button type="reset">reset</button>
 
                     </div>
 
